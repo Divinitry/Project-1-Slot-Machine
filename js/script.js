5134d622a672025b8a65712a637283c769dcff46
SMcGoey <SpencerCodeAcc@gmail.com>
2024-05-06 21:39:44 -0400

    layout finished - dynamic updating and buttons all done, start a good portion of the logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,92 @@
+// Global variables
+let currentMoney = 100;
+let wager5 = 5;
+let wager10 = 10;
+let wager15 = 15;
+let wager20 = 20;
+let wager25 = 25;
+let wager100 = 100;
+
+const moneyDisplay = document.getElementById("money-display");
+moneyDisplay.innerText = `$${currentMoney}`;
+
+const confirmButton = document.getElementById("confirm-button");
+confirmButton.disabled = true;
+
+const slotButton = document.getElementById("coin-slot")
+slotButton.disabled = true;
+
+const lever = document.getElementById("lever")
+lever.style.pointerEvents = "none";
+
+const fiveButton = document.getElementById("5DollarButton");
+const tenButton = document.getElementById("10DollarButton");
+const fifteenButton = document.getElementById("15DollarButton");
+const twentyButton = document.getElementById("20DollarButton");
+const twentyFiveButton = document.getElementById("25DollarButton");
+const oneHundredButton = document.getElementById("100DollarButton");
+
+function pauseButtons(){
+    fiveButton.disabled = true;
+    tenButton.disabled = true;
+    fifteenButton.disabled = true;
+    twentyButton.disabled = true;
+    twentyFiveButton.disabled = true;
+    oneHundredButton.disabled = true;
+}
+
+fiveButton.addEventListener("click", function () {
+    buttonHandler(wager5);
+});
+tenButton.addEventListener("click", function () {
+    buttonHandler(wager10);
+});
+fifteenButton.addEventListener("click", function () {
+    buttonHandler(wager15);
+});
+twentyButton.addEventListener("click", function () {
+    buttonHandler(wager20);
+});
+twentyFiveButton.addEventListener("click", function () {
+    buttonHandler(wager25);
+});
+oneHundredButton.addEventListener("click", function () {
+    buttonHandler(wager100);
+});
+
+function buttonHandler(wager) {
+    selectedWager = wager
+    if (currentMoney >= selectedWager) {
+        confirmButton.disabled = false; 
+    } else {
+        alert(`Not enough money to wager $${selectedWager} dollars, you currently have $${currentMoney}`);
+        // CHANGE THIS TO AN ACTUAL NICE LOOKING ALERT! ^
+    }
+    confirmButton.addEventListener("click", function(){
+        currentMoney -= selectedWager;
+        moneyDisplay.innerText = `$${currentMoney}`;
+        slotButton.disabled = false;
+        confirmButton.disabled = true;
+        pauseButtons()
+        selectedWager = 0
+    })
+}
+
+lever.addEventListener("click", function(){
+    document.getElementById("lever").style.display = "none";
+    document.getElementById("pulledlever").style.display = "flex";
+    
+    setInterval(() => {
+    document.getElementById("lever").style.display = "flex";
+    document.getElementById("pulledlever").style.display = "none";
+    }, 300)
+   })
+
+slotButton.addEventListener("click", function(){
+    lever.style.pointerEvents = "auto";
+})
+
+
 /* 
 
 update document.get for when u make the lever and make each slot display different fruits when it spins, just + 1 index and change the emojis
